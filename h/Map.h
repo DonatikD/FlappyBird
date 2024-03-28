e17ae7e0801f72ed88857376485218004252ea51
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2024-03-28 19:17:43 +0200

    Update Map.h

--- h/Map.h
+++ h/Map.h
@@ -5,31 +5,54 @@ class Map
 
 public:
 	std::string mapFileName;
-	static const int mapHeigth = 15;
-	static const int mapWidth = 20;
+	std::string mapDecorationFileName;
+	static const int mapHeigth = 27;
+	static const int mapWidth = 52;
 	char mapField[mapHeigth][mapWidth]{
-		{"                   "},	
-    	{"                   "},
-		{"                   "},
-		{"                   "},
-		{"                   "},
-		{"                   "},
-    	{"                   "},
-		{"                   "},
-		{"                   "},
-		{"                   "},
-		{"                   "},
-		{"                   "},
-		{"                   "},
-		{"                   "},
-		{"                   "},
+	    {"                                                   "},
+        {"                                                   "},
+	    {"                                                   "},
+	    {"                                                   "},
+	    {"                                                   "},
+	    {"                                                   "},
+	    {"                                                   "},
+	    {"                                                   "},
+	    {"                                                   "},
+	    {"                                                   "},
+	    {"                                                   "},
+	    {"                                                   "},
+	    {"                                                   "},
+		{"                                                   "},
+	    {"                                                   "},
+	    {"777777777777777777777777777777777777777777777777777"},
+	    {"777777777777777777777777777777777777777777777777777"},
+		{"777777777777777777777777777777777777777777777777777"},
+		{"          2            2           2      2        "},
+		{"          2            2           2      2        "},
+		{"          2            2           2      2        "},
+		{"          3      6     3       6   3      3     6  "},
+        {"    5  6        5              5             5     "},
+		{"    4           4              4             4     "},
+		{"    4     6     4      6       4      6      4     "},
+	    {"    4           4              4             4     "},
+		{"111111111111111111111111111111111111111111111111111"},
+	
+	
 	};
-	Map(std::string mapFileName);
+	Map(std::string mapFileName, std::string decorationFileName);
 	void mapLoad(std::string mapFileName);
 	void mapShow(sf::RenderWindow& window);
-	sf::Vector2f mapTileSize = { 2304.0f, 864.0f };
+	/*sf::Vector2f mapTileSize = { 2304.f,864.f };
+	sf::Image mapImage;
+	sf::Texture mapTexture;
+	sf::Sprite mapSprite;*/
+
+	sf::Vector2f mapTileSize = { 2304.f, 864.f };
 	sf::Image mapImage;
 	sf::Texture mapTexture;
 	sf::Sprite mapSprite;
+	sf::Texture decorationTexture;
+	
+
 
 };
