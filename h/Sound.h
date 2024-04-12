f91d757b0694f08954fe5fd54f6913bf8ea6908b
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2024-04-12 20:42:36 +0300

    Update Sound.h

--- h/Sound.h
+++ h/Sound.h
@@ -4,16 +4,17 @@
 class Sound {
 private:
     sf::Music music;
-
+    float volume = 100.0f; // Початкове значення гучності
 public:
     Sound();
     ~Sound();
-
+  
     void loadMusic(const std::string& filename);
     void play();
     void pause();
     void stop();
     void setLoop(bool loop);
     void setVolume(int volume);
+    float getVolume()const;
 };
 
