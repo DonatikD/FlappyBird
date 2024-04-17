709d2755fcc5d9136f04e38a4cdab515ab4f6567
DonatikD <153946680+DonatikD@users.noreply.github.com>
2024-04-17 22:39:31 +0300

    Update Sprite.h

--- h/Sprite.h
+++ h/Sprite.h
@@ -1,6 +1,3 @@
-
-
-
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <string>
@@ -10,18 +7,28 @@
 
 using namespace std;
 class Player {
+private:
+	enum Direction { STAND, RIGHT, UP, DOWN };
+	bool gameOver = false;
+	bool jumping = true;
+	bool falling = true;
+	double jumpVelocity = -0.000005f;
+	double gravity = -0.005f;
+	double gravityFall = 0.000005f;
+	double fallingVelocity = 0.005f;
 public:
 	std::string playerFileName;
 	double playerSpeed = 50.f / 300;
 	double animationTime = 0.f;
 	int animationFrameNumber = 0;
 	Player(std::string playerFileName);
-	void showPlayer(sf::RenderWindow& window);
-	void moveUp(double timer, Camera& camera, Map map);
-	void moveLeft(double timer, Camera& camera, Map map);
+	void showPlayer(sf::RenderWindow& window) const;
+	//void update(double timer, Camera& camera, Map map);
+	void jump(double timer, Camera& camera, Map map);
 	void moveRight(double timer, Camera& camera, Map map);
 	void moveDown(double timer, Camera& camera, Map map);
-	enum Direction { STAND = 0, UP, DOWN, LEFT, RIGHT };
+	void fallFalse();
+	void fallTrue();
 	Direction playerDirection = STAND;
 	sf::Image playerImage;
 	sf::Texture playerTexture;
