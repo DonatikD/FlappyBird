7bd7d824f2387ea42c7f882ce026fc314461e724
pawok <153946151+p4wok@users.noreply.github.com>
2024-04-17 20:40:32 +0300

    Camera.cpp

--- Camera/Camera.cpp
+++ Camera/Camera.cpp
@@ -1,20 +1,20 @@
 #include "Camera.h"
 
-
-
-Camera::Camera(sf::Vector2u screenSize, sf::Vector2f playerPosition) :screenSize(screenSize) {
-    cameraSize = { screenSize.x / 2.3f,screenSize.y / 2.3f };
+Camera::Camera(sf::Vector2u screenSize, sf::Vector2f playerPosition) : screenSize(screenSize) {
+    cameraSize = { screenSize.x / 2.3f, screenSize.y / 2.3f };
     view.reset(sf::FloatRect(0, 0, cameraSize.x, cameraSize.y));
     view.setCenter(playerPosition);
     correctionPosition(playerPosition);
 }
 
 void Camera::SetCentrePosition(sf::Vector2f playerPosition) {
-    view.setCenter(playerPosition);
+    sf::Vector2f currentCenter = view.getCenter();
+    view.setCenter(playerPosition.x, currentCenter.y); 
     correctionPosition(playerPosition);
 }
+
 void Camera::correctionPosition(sf::Vector2f playerPosition) {
-    sf::Vector2f cameraPos = playerPosition;
+    sf::Vector2f cameraPos = view.getCenter(); 
     if (cameraPos.y < (view.getSize().y / 2.f))
         cameraPos.y = (view.getSize().y / 2);
     if (cameraPos.y > (screenSize.y - (view.getSize().y / 2.f)))
@@ -25,24 +25,15 @@ void Camera::correctionPosition(sf::Vector2f playerPosition) {
     if (cameraPos.x > (screenSize.x - cameraSize.x / 2.f))
         cameraPos.x = (screenSize.x - cameraSize.x / 2.f);
 
-
     view.setCenter(cameraPos);
 }
 
-
 void Camera::CameraZoomUp(sf::Vector2f playerPosition) {
     view.zoom(1.1);
-    view.setCenter(playerPosition);
-    correctionPosition(playerPosition);
+    SetCentrePosition(playerPosition); 
 }
-
 
 void Camera::CameraZoomDown(sf::Vector2f playerPosition) {
     view.zoom(0.9);
-    view.setCenter(playerPosition);
-    correctionPosition(playerPosition);
+    SetCentrePosition(playerPosition); 
 }
-
-
-
-//testt
