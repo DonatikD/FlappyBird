04255530c444ea89f9a964102a1509fb84b1f8a2
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2024-04-24 20:51:22 +0300

    Update Options.h

--- h/Options.h
+++ h/Options.h
@@ -1,10 +1,8 @@
 
-#ifndef OPTIONS_H
 #define OPTIONS_H
 
 #include <SFML/Graphics.hpp>
 #include "MainMenu.h"
-
 //namespace options {
 //    void runOptions() {
 //        sf::RenderWindow optionsWindow(sf::VideoMode(1920, 1080), L"OPTIONS");
@@ -30,25 +28,79 @@
 //        }
 //    }
 //}
-
 #include <SFML/Audio.hpp>
+#include <iostream>
+#include "Map.h"
+#include "Sound.h"
 
+
+using namespace std;
 namespace options {
     void runOptions() {
         sf::RenderWindow optionsWindow;
-
         optionsWindow.create(sf::VideoMode(1920, 1080), L"Options");
-       /* sf::RenderWindow optionsWindow(sf::VideoMode(1920, 1080), L"OPTIONS");*/
         sf::Clock mainGameClock;
         sf::Time gameTimer;
-        Map mainMap1("MenuBG.png", "SPRITES_5.png");
 
-        sf::SoundBuffer buffer;
-        buffer.loadFromFile("Sound/Music1.mp3");
-        sf::Sound sound;
-        sound.setBuffer(buffer);
+        sf::Text text;
+        text.setString("Developed by: Baran Maksym");
+        text.setCharacterSize(30); // Розмір шрифту
+
+        text.setPosition(1000, 800);
+        text.setFillColor(sf::Color::Red);
+
+
+      
+        Sound sound;
+        sound.loadMusic("Sound/Music1.mp3");
+        sound.setVolume(5);
         sound.play();
-        bool soundOn = true;
+
+        bool soundOn = false;
+
+
+        sf::Font font;
+        if (!font.loadFromFile("Fonts/Silkscreen [UA by Mr.Enot].ttf")) {
+            std::cerr << "Error loading font\n";
+          return;
+        }
+
+        sf::Text Dtext;
+        Dtext.setFont(font);
+        Dtext.setString("Developed by:");
+        Dtext.setCharacterSize(24);
+        Dtext.setFillColor(sf::Color::Black);
+        Dtext.setPosition(750,900);
+
+
+
+        sf::Text Mtext;
+        Mtext.setFont(font);
+        Mtext.setString("Maksym Baran");
+        Mtext.setCharacterSize(24);
+        Mtext.setFillColor(sf::Color::Black);
+        Mtext.setPosition(930, 850);
+
+
+
+
+        sf::Text Otext;
+        Otext.setFont(font);
+        Otext.setString("Options");
+        Otext.setCharacterSize(70);
+        Otext.setFillColor(sf::Color::Black);
+        Otext.setPosition(780, 100);
+
+
+
+        sf::Texture backgroundTexture;
+        if (!backgroundTexture.loadFromFile("Photo/MenuBG1.png")) {
+            return;
+        }
+        sf::Sprite backgroundSprite(backgroundTexture);
+
+
+
 
         while (optionsWindow.isOpen()) {
             gameTimer = mainGameClock.restart();
@@ -73,13 +125,16 @@ namespace options {
                     }
                 }
             }
-            optionsWindow.clear(sf::Color::Blue);
-         /*   mainMap1.mapShow(optionsWindow);*/
+         
+            optionsWindow.clear();           
+            optionsWindow.draw(backgroundSprite);
+            optionsWindow.draw(Dtext);
+            optionsWindow.draw(Mtext);
+            optionsWindow.draw(Otext);
             optionsWindow.display();
         }
     }
-  
+
 }
 
 
-#endif // OPTIONS_H
