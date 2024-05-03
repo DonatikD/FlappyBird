280e4a5af844617f57bbe98f4dfd7dea48bcb465
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2024-05-03 22:17:59 +0300

    Update MainMenu.h

--- h/MainMenu.h
+++ h/MainMenu.h
@@ -1,3 +1,43 @@
+//#pragma once
+//
+//#include <SFML/Graphics.hpp>
+//#include "Button.h"
+//
+//class MainMenu {
+//private:
+//    sf::RenderWindow window;
+//    Button button;
+//    sf::Sprite menuBG;
+//    sf::Sprite FlappyBirdT;
+//    sf::Sprite Button1Sprite;
+//    sf::Texture backTexture;
+//    sf::Texture FlappyBirdTexture;
+//    sf::Texture buttonTexture;
+//    sf::Font font;
+//
+//public:
+//    MainMenu();
+//    void run();
+//
+//};
+
+//#include <SFML/Graphics.hpp>
+//#include "Button.h"
+//
+//class MainMenu {
+//private:
+//    sf::RenderWindow window;
+//    Button button, buttonEXIT,/*buttonDevelopers*/buttonOptions;
+//    sf::Sprite menuBG, FlappyBirdT, Button1Sprite, Button2Sprite, Button3Sprite, Button4Sprite;
+//    sf::Texture backTexture, buttonTextureEXIT, FlappyBirdTexture, buttonTexture, ButtonDevelopersTexture, ButtonOptionsTexture;
+//    sf::Font font;
+//
+//public:
+//    MainMenu();
+//    void run();
+//
+//};
+
 #pragma once
 
 #include <SFML/Graphics.hpp>
@@ -7,9 +47,9 @@
 class MainMenu {
 private:
     sf::RenderWindow window;
-    Button button, buttonEXIT,/*buttonDevelopers*/buttonOptions;
+    Button button, buttonEXIT,buttonOptions/*,buttonDevelopers*/;
     sf::Sprite menuBG, FlappyBirdT, Button1Sprite, Button2Sprite, Button3Sprite, Button4Sprite;
-    sf::Texture backTexture, buttonTextureEXIT, FlappyBirdTexture, buttonTexture, ButtonDevelopersTexture, ButtonOptionsTexture;
+    sf::Texture backTexture, buttonTextureEXIT, FlappyBirdTexture, buttonTexture, /*ButtonDevelopersTexture,*/ ButtonOptionsTexture;
     sf::Font font;
     Sound music;
 
