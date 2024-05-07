c8aeecd6ece8b9e7deb1668457b59f872b126835
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2024-05-08 00:30:08 +0300

    Update Music.h

--- h/Music.h
+++ h/Music.h
@@ -30,6 +30,12 @@ namespace Music {
         sf::Sprite DeveSprite(DeveTexture);
 
 
+        Sound sound;
+        sound.loadMusic("Sound/Music1.mp3");
+        sound.setVolume(100);
+        sound.play();
+
+        bool soundOn = false;
 
 
         sf::Font font;
@@ -38,48 +44,71 @@ namespace Music {
             return;
         }
 
+        sf::Text Otext;
+        Otext.setFont(font);
+        Otext.setString("Music Options");
+        Otext.setCharacterSize(85);
+        Otext.setFillColor(sf::Color::White);
+        Otext.setPosition(630, 100);
 
-        sf::Text Devtext;
+        Button buttonChangeMusic(790, 350, 500, 200);
 
+        sf::Texture buttonTexture;
+        if (!buttonTexture.loadFromFile("Photo/ChangeSound.png")) {
+            return;
+        }
 
-        Devtext.setFont(font);
-        Devtext.setString("Card numbers");
-        Devtext.setCharacterSize(50);
-        Devtext.setFillColor(sf::Color::White);
-        Devtext.setPosition(750, 150);
+        sf::Sprite buttonSprite(buttonTexture);
+        buttonChangeMusic.setTexture(buttonTexture);
 
-
-        sf::Text Devtext1;
-
-
-        Devtext1.setFont(font);
-        Devtext1.setString("Bay Sergiy  (DonatikD)");
-        Devtext1.setCharacterSize(50);
-        Devtext1.setFillColor(sf::Color::White);
-        Devtext1.setPosition(600, 300);
-
+        buttonSprite.setPosition(750, 400);
 
 
-        sf::Text Devtext2;
+        Button buttonSoundOff(790, 500, 500, 200);
+
+        sf::Texture buttonSoundOffTexture;
+        if (!buttonSoundOffTexture.loadFromFile("Photo/SoundOFF.png")) {
+            return;
+        }
+
+        sf::Sprite buttonOffSprite(buttonSoundOffTexture);
+        buttonSoundOff.setTexture(buttonSoundOffTexture);
+
+        buttonOffSprite.setPosition(750, 400);
 
 
-        Devtext2.setFont(font);
-        Devtext2.setString("Kyslov Pavlo  (pwk.exe)");
-        Devtext2.setCharacterSize(50);
-        Devtext2.setFillColor(sf::Color::White);
-        Devtext2.setPosition(600, 400);
+
+    
+        Button buttonSoundM(600, 650, 500, 200);
+
+        sf::Texture buttonMTexture;
+        if (!buttonMTexture.loadFromFile("Photo/MusicMinus.png")) {
+            return;
+        }
+
+        sf::Sprite buttonMSprite(buttonMTexture);
+        buttonSoundM.setTexture(buttonMTexture);
+
+        buttonMSprite.setPosition(750, 400);
 
 
 
-        sf::Text Devtext3;
+        Button buttonSoundA(950, 650, 500, 200);
+
+        sf::Texture buttonATexture;
+        if (!buttonATexture.loadFromFile("Photo/MusicAdd.png")) {
+            return;
+        }
+
+        sf::Sprite buttonASprite(buttonATexture);
+        buttonSoundA.setTexture(buttonATexture);
+
+        buttonASprite.setPosition(750, 400);
 
 
-        Devtext3.setFont(font);
-        Devtext3.setString("Maksym Baran  (Jacobs)");
-        Devtext3.setCharacterSize(50);
-        Devtext3.setFillColor(sf::Color::White);
-        Devtext3.setPosition(600, 500);
+        int currentMusicIndex = 0;
 
+        std::vector<std::string> musicPaths = { "Sound/Music1.mp3","Sound/chamgeMusic.mp3" };
 
         while (MusicWindow.isOpen()) {
             gameTimer = mainGameClock.restart();
@@ -95,6 +124,71 @@ namespace Music {
                     menu.run();
                 }
 
+                buttonChangeMusic.update(MusicWindow);
+
+
+                if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2f mousePosition = sf::Vector2f(event.mouseButton.x, event.mouseButton.y);
+
+
+                    if (buttonChangeMusic.isMouseOver(MusicWindow)) {
+                        sound.pause();
+                        currentMusicIndex++;
+                        if (currentMusicIndex >= musicPaths.size()) {
+                            currentMusicIndex = 0;
+                        }
+                        sound.loadMusic(musicPaths[currentMusicIndex]);
+                        sound.setVolume(30);
+                        sound.play();
+                    }
+                }
+                
+
+                buttonSoundOff.update(MusicWindow);
+
+
+                if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2f mousePosition = sf::Vector2f(event.mouseButton.x, event.mouseButton.y);
+                    if (buttonSoundOff.isMouseOver(MusicWindow)) {
+                        std::cout << " button Off clicked\n";
+                        sound.pause();
+
+
+                    }
+                }
+
+
+                buttonSoundM.update(MusicWindow);
+
+
+                if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2f mousePosition = sf::Vector2f(event.mouseButton.x, event.mouseButton.y);
+                    if (buttonSoundM.isMouseOver(MusicWindow)) {
+                        std::cout << " button Minus clicked\n";
+                        int volume = sound.getVolume();
+                        volume -= 10; // Зменшуємо гучність на 30
+                        if (volume < 0) volume = 0; // Гарантуємо, що гучність не від'ємна
+                        sound.setVolume(volume);
+                    }
+                }
+
+
+
+
+
+                buttonSoundA.update(MusicWindow);
+
+
+                if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2f mousePosition = sf::Vector2f(event.mouseButton.x, event.mouseButton.y);
+                    if (buttonSoundA.isMouseOver(MusicWindow)) {
+                        std::cout << " button Add clicked\n";
+                        int volume = sound.getVolume();
+                        volume += 10; // Збільшуємо гучність на 30
+                        if (volume > 100) volume = 100; // Гарантуємо, що гучність не більша за 100
+                        sound.setVolume(volume);
+                    }
+                }
 
             }
 
@@ -103,10 +197,11 @@ namespace Music {
 
             MusicWindow.clear();
             MusicWindow.draw(DeveSprite);
-           /* MusicWindow.draw(Devtext);
-            MusicWindow.draw(Devtext1);
-            MusicWindow.draw(Devtext2);*/
-    /*        DonateWindow.draw(Devtext3);*/
+            MusicWindow.draw(Otext);
+            buttonChangeMusic.draw(MusicWindow);
+            buttonSoundOff.draw(MusicWindow);
+            buttonSoundM.draw(MusicWindow);
+            buttonSoundA.draw(MusicWindow);
             MusicWindow.display();
         }
     }
